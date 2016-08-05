8f7dea46c92685cc2b7175a3ce1c63c0d9454144
aishwarya <aishwaryacshankar@gmail.com>
2016-08-05 15:06:19 -0700

    sqllearnings commit

---
+++ boutique.sql
@@ -0,0 +1,17 @@
+/*aishwarya*/
+CREATE TABLE Boutique
+(
+Account int,
+    Name varchar(255),
+    Dress varchar(255));
+    
+INSERT INTO Boutique(Account,Name,Dress)
+VALUES (1,'ashlee','gagra');
+
+SELECT * FROM Boutique;
+
+Update Boutique
+SET Dress ='saree'
+WHERE Account=1;
+
+SELECT * FROM Boutique
---
+++ sqlpractice.sql
@@ -0,0 +1,19 @@
+
+/*aishwarya*/
+CREATE TABLE Movies2
+(
+TicketNo int,
+MovieName varchar(255),
+Theater varchar(255)
+);
+
+INSERT INTO Movies2 (TicketNo,MovieName,Theater)
+VALUES (101,'gentleman','amc');
+
+SELECT * FROM Movies2;
+
+UPDATE Movies2
+SET MovieName='Kala Chashmaa'
+WHERE 101;
+
+SELECT * FROM Movies2;
